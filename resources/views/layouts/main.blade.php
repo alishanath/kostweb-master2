868d8a60cbdb57844edb728f92233009fa2f4542
Alisha Nathania Septianty <alishanathaniaseptiantty@gmail.com>
2025-07-31 21:12:38 +0700

    tes-1

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -215,7 +215,7 @@
             </div>
         </div>
         <div class="container mt-4">
-            <p>&copy; 2017 Kost Putri Alfia Purwokerto.</p>
+            <p>&copy; 2025 Kost Putri Alfia Purwokerto.</p>
         </div>
     </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
